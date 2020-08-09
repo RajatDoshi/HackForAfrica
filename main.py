2e08e178372f934d4f3f0a6fa89b5aab1944f2f9
Rajat Doshi <rajatdoshi@hotmail.com>
2020-08-09 09:57:57 -0400

    Made Landing Page

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)             
 
 @app.route("/")                   
-def hello():                     
-    return "Hello World!"   
+def home():                     
+    return render_template('home.html')
+
+@app.route("/askQuestions")                   
+def askQuestions():                     
+    return render_template('askQuestions.html')
+
+@app.route("/answerQuestions")                   
+def answerQuestions():                     
+    return render_template('answerQuestions.html')
+
+
 
 if __name__ == "__main__":        
     app.run()                     
